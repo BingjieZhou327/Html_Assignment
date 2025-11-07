8799f0cddf2a00909b94c0efacbdcef75383d395
BingjieZhou327 <zhou.bingji@northeastern.edu>
2025-11-07 14:28:28 +0800

    Create Express server with middleware setup

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+require('dotenv').config();
+
+const connectDB = require('./config/database');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static images
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// Swagger documentation
+const swaggerDocument = YAML.load(path.join(__dirname, 'swagger', 'swagger.yaml'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Routes
+app.use('/user', userRoutes);
+
+// Root endpoint
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to User Management API',
+    documentation: `http://localhost:${PORT}/api-docs`
+  });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`API Documentation available at http://localhost:${PORT}/api-docs`);
+});
+
+module.exports = app;
+
