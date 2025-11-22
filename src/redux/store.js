cb48ed78b7426c537ef6fdd163fc056316437a85
BingjieZhou327 <zhou.bingji@northeastern.edu>
2025-11-22 22:28:00 +0800

    Configure Redux store and integrate with React app

---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
+import usersReducer from './slices/usersSlice';
+import jobsReducer from './slices/jobsSlice';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    users: usersReducer,
+    jobs: jobsReducer,
+  },
+});
+
+export default store;
+
